af783d7a3cfddb35f055effa47ab72bea28dd492
李春凯 <2921905842@qq.com>
2023-06-08 11:51:28 +0800

    将找出字符串中最长的回文子串添加

--- 题库/test/Main.java
+++ 题库/test/Main.java
@@ -5,25 +5,36 @@ import java.util.Scanner;
 /**
  * Created by IntelliJ IDEA.
  * Author：Lick
- * Date Created in 2023-05-23
- * Time：19:48
+ * Date Created in 2023-06-08
+ * Time：11:50
  */
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
-        StringBuilder sb = new StringBuilder(str);
-        sb.reverse();
-        for (int i = 0; i < sb.length(); i++) {
-            char ch = sb.charAt(i);
-            if (Character.isUpperCase(ch)) {
-                sb.setCharAt(i, Character.toLowerCase(ch));
-            } else if (Character.isLowerCase(ch)) {
-                sb.setCharAt(i, Character.toUpperCase(ch));
+        int n = str.length();
+        boolean[][] dp = new boolean[n][n];
+        String res = "";
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (len == 1) {
+                    dp[i][j] = true;
+                } else {
+                    boolean b = str.charAt(i) == str.charAt(j);
+                    if (len == 2) {
+                        dp[i][j] = b;
+                    } else {
+                        dp[i][j] = (b && dp[i + 1][j - 1]);
+                    }
+                }
+                if (dp[i][j] && len > res.length()) {
+                    res = str.substring(i, j + 1);
+                }
             }
         }
-        System.out.println(sb.toString());
+        System.out.println(res);
     }
 }
 
---
+++ 题库/test/Test010.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author：Lick
+ * Date Created in 2023-06-08
+ * Time：11:47
+ */
+public class Test010 {
+}
