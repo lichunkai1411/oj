075c46f835a735f1075b972231d21da6e089913b
李春凯 <2921905842@qq.com>
2023-08-25 13:52:12 +0800

    更新代码

--- 牛客/牛客语法题库/Test01.java
+++
@@ -1,7 +0,0 @@
-package 牛客语法题库;
-
-public class Test01 {
-    public static void main(String[] args) {
-        System.out.println("Hello Nowcoder!");
-    }
-}
--- 题库/test/Main.java
+++ 题库/test/Main.java
@@ -1,5 +1,53 @@
 package test;
 
-public class Main {
-
+// 文本接口
+interface Text {
+    String getContent();
 }
+
+// 基本文本类
+class SimpleText implements Text {
+    private String content;
+
+    public SimpleText(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+}
+
+// 装饰器抽象类
+abstract class TextDecorator implements Text {
+    protected Text decoratedText;
+
+    public TextDecorator(Text decoratedText) {
+        this.decoratedText = decoratedText;
+    }
+}
+
+// 删除文本装饰器
+class DeleteTextDecorator extends TextDecorator {
+    public DeleteTextDecorator(Text decoratedText) {
+        super(decoratedText);
+    }
+
+    @Override
+    public String getContent() {
+        return "<del>" + decoratedText.getContent() + "</del>";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Text simpleText = new SimpleText("Hello, world!");
+        Text deletedText = new DeleteTextDecorator(simpleText);
+
+        System.out.println("Simple Text: " + simpleText.getContent());
+        System.out.println("Deleted Text: " + deletedText.getContent());
+
+    }
+}
+
